00459549abed9284e228790bb2a3005bbd3759ed
Pj0hnston <72583642+Pj0hnston@users.noreply.github.com>
2020-10-10 17:50:24 -0400

    Added content to the body

--- src/page-parts/body/index.js
+++ src/page-parts/body/index.js
@@ -12,7 +12,23 @@ function Body(props) {
 					<hr />
 					<div id="body__text">
 						<h2>Mission Statement</h2>
-						<ul><li>ReLearn is designed with users in mind to help their tutoring experience</li></ul>
+						<ul><li>
+						Relearn is designed with users in mind to help their tutoring experience. 
+						Whenever learning a new topic it can be just as hard to learn the teaching style of the person conducting the course. 
+						We wanted to help students find the best tutors who not only know the material, but how to connect with the student to better learn the material.
+						</li></ul>
+
+						<h2>Personality Matching</h2>
+						<ul><li>
+						With our personality matching algorithm combined with a machine learning model trained for semantic analysis, we are able to perfectly match the tutor 
+						to your personality. This gives a much higher chance at not only connecting with the tutor, but to improve the way you would learn the material!
+						</li></ul>
+
+						<h2>Real Time Comparisons</h2>
+						<ul><li>
+						With our machine learning methods, we can compare your personality to other tutors in real time! This helps connect to a tutor as soon as they become available.
+						</li></ul>
+						
 					</div>
 				</div>
 			</div>
