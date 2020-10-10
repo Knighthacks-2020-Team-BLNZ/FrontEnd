50ff190a52b6190894eeab49c37ef0ff9fb430b6
Zackary Santana <zackzackyack@gmail.com>
2020-10-10 10:44:27 -0400

    Redesigned main index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom';
 
 // Components
 import LangingPage from './landing-page';
-import NavBar from './nav';
 
 
 // Styles
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <LangingPage>
-      <NavBar />
-    </LangingPage>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<LangingPage />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
