c618aaf24509933cb2aa4ef37971d062717d58e3
Zackary Santana <zackzackyack@gmail.com>
2020-10-09 22:27:25 -0400

    Added coded for logged in nav and logged out

--- src/nav/index.js
+++ src/nav/index.js
@@ -4,15 +4,39 @@ import React from 'react';
 import  './index.css';
 
 function Nav(props) {
+    const LoggedIn = <LoggedIn />;
+    const Loggedout = <LoggedOut />;
 
-    const logged_out_nav = (
+    return (
+        <>
+        {props.logged_in ? LoggedIn : LoggedOut}
+        </>
+    )
+}
+
+function LoggedIn(props) {
+    <ul>
+        <li onClick={props.handle_logout}>
+            Logout
+        </li>
+    </ul>
+}
+
+function LoggedOut(props) {
+    return (
         <ul>
-            <li onClick={() => {
-                props.display_form()
-            }}>
+            <li onClick={(
+                props.display_form('login')
+            )}>
                 Login
+            </li>
+            <li onClick={(
+                props.display_form('Register')
+            )}>
+                Register
             </li>
         </ul>
     )
-
 }
+
+export default Nav;
