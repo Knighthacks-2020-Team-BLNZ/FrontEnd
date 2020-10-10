1848fc76b52f7bec70ce0f3842c7e95df3120f50
Pj0hnston <72583642+Pj0hnston@users.noreply.github.com>
2020-10-10 13:16:33 -0400

    Added some text to body that can be"buttons"

--- src/page-parts/body/index.js
+++ src/page-parts/body/index.js
@@ -7,6 +7,19 @@ function Body(props) {
 	return (
 		<>
 			<div id="body">
+			<p><center> 
+				Welcome Back to ReLearn!. We strive to help connect tutors with you based on a combination of both the topic you want to learn, and the best personality
+				that matches with yours! <br></br>
+
+				Lets get started
+			</center></p>
+			
+			<h2><center>View your current Tutors</center></h2>
+			<h2><center>Match with a Tutor</center></h2>
+			<h2><center>Match with a Tutor based on Subject</center></h2>
+			<h2><center>Match with a Tutor based on personality</center></h2>
+
+
 			</div>
 		</>
 	)
